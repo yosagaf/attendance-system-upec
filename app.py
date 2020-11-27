d9af7f79424dae0ddb0ac84a2bfb753e9516a6a0
yosagaf <sagafysf@gmail.com>
2020-11-27 10:01:13 +0100

    exclude some files while pushing

--- app.py
+++ app.py
@@ -1,8 +1,51 @@
+
+import os
+import sys
+import face_recognition
 import cv2
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 import numpy as np
+from face_reco import *
 
-def main():
-    print("Hello")
+class MyForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-if __name__ == '__main__':
-    main()
+        self.ui.startPushButton.clicked.connect(self.capturing)
+        self.ui.stopPushButton.clicked.connect(self.stopping)
+        #self.ui.progressBar.setValue(0)
+
+    def Display(self, frame, label):
+        
+        print("Donnnnnnnnnnnne")
+        print(frame.shape)
+        '''
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frameb = QImage(image.data, image.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+        pix = QPixmap.fromImage(frameb)
+        resized = pix.scaled(label.width(), label.height(), QtCore.Qt.KeepAspectRatio)
+        label.setPixmap(resized)
+        '''
+        
+
+    def stopping(self):
+        print("Done")
+
+    def capturing(self):
+        self.cap = cv2.VideoCapture(0)
+        while True:
+            ret, self.frame = self.cap.read()
+            if ret == True:
+                self.Display(self.frame, self.ui.displayResultLabel)
+                cv2.waitKey(1)
+            else:
+                break
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    w = MyForm()
+    w.show()
+    sys.exit(app.exec_())
--- face_reco.py
+++ face_reco.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gui.ui'
+# Form implementation generated from reading ui file 'gui2.ui'
 #
 # Created by: PyQt5 UI code generator 5.12
 #
@@ -9,73 +9,69 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(1204, 851)
-        self.groupBox = QtWidgets.QGroupBox(Dialog)
-        self.groupBox.setGeometry(QtCore.QRect(10, -10, 1181, 851))
-        self.groupBox.setTitle("")
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1319, 839)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(-40, -100, 1351, 891))
         self.groupBox.setObjectName("groupBox")
-        self.logoLabel = QtWidgets.QLabel(self.groupBox)
-        self.logoLabel.setGeometry(QtCore.QRect(20, 30, 151, 91))
-        self.logoLabel.setText("")
-        self.logoLabel.setObjectName("logoLabel")
-        self.videoGroupBox = QtWidgets.QGroupBox(self.groupBox)
-        self.videoGroupBox.setGeometry(QtCore.QRect(370, 130, 791, 711))
-        self.videoGroupBox.setAlignment(QtCore.Qt.AlignCenter)
-        self.videoGroupBox.setObjectName("videoGroupBox")
-        self.videoLabel = QtWidgets.QLabel(self.videoGroupBox)
-        self.videoLabel.setGeometry(QtCore.QRect(0, 20, 801, 641))
-        self.videoLabel.setText("")
-        self.videoLabel.setObjectName("videoLabel")
-        self.startBtn = QtWidgets.QPushButton(self.groupBox)
-        self.startBtn.setGeometry(QtCore.QRect(140, 170, 89, 25))
-        self.startBtn.setObjectName("startBtn")
-        self.labelTheme = QtWidgets.QLabel(self.groupBox)
-        self.labelTheme.setGeometry(QtCore.QRect(470, 50, 601, 61))
+        self.startPushButton = QtWidgets.QPushButton(self.groupBox)
+        self.startPushButton.setGeometry(QtCore.QRect(180, 250, 111, 31))
+        self.startPushButton.setObjectName("startPushButton")
+        self.stopPushButton = QtWidgets.QPushButton(self.groupBox)
+        self.stopPushButton.setGeometry(QtCore.QRect(180, 290, 111, 31))
+        self.stopPushButton.setObjectName("stopPushButton")
+        self.displayFrameGroupBox = QtWidgets.QGroupBox(self.groupBox)
+        self.displayFrameGroupBox.setGeometry(QtCore.QRect(490, 200, 851, 681))
+        self.displayFrameGroupBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.displayFrameGroupBox.setTitle("")
+        self.displayFrameGroupBox.setObjectName("displayFrameGroupBox")
+        self.displayResultGoupBox = QtWidgets.QGroupBox(self.groupBox)
+        self.displayResultGoupBox.setGeometry(QtCore.QRect(50, 320, 421, 561))
+        self.displayResultGoupBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.displayResultGoupBox.setObjectName("displayResultGoupBox")
+        self.displayResultLabel = QtWidgets.QLabel(self.displayResultGoupBox)
+        self.displayResultLabel.setGeometry(QtCore.QRect(10, 30, 401, 511))
+        self.displayResultLabel.setText("")
+        self.displayResultLabel.setObjectName("displayResultLabel")
+        self.textLabel = QtWidgets.QLabel(self.groupBox)
+        self.textLabel.setGeometry(QtCore.QRect(660, 170, 501, 31))
         font = QtGui.QFont()
-        font.setPointSize(18)
-        self.labelTheme.setFont(font)
-        self.labelTheme.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
-        self.labelTheme.setObjectName("labelTheme")
-        self.resultGroupBox = QtWidgets.QGroupBox(self.groupBox)
-        self.resultGroupBox.setGeometry(QtCore.QRect(20, 280, 331, 561))
-        self.resultGroupBox.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.resultGroupBox.setAlignment(QtCore.Qt.AlignCenter)
-        self.resultGroupBox.setObjectName("resultGroupBox")
-        self.resultLabel = QtWidgets.QLabel(self.resultGroupBox)
-        self.resultLabel.setGeometry(QtCore.QRect(0, 20, 331, 521))
-        self.resultLabel.setText("")
-        self.resultLabel.setAlignment(QtCore.Qt.AlignCenter)
-        self.resultLabel.setObjectName("resultLabel")
-        self.logLabel = QtWidgets.QLabel(self.groupBox)
-        self.logLabel.setGeometry(QtCore.QRect(10, 20, 281, 111))
-        self.logLabel.setText("")
-        self.logLabel.setTextFormat(QtCore.Qt.RichText)
-        self.logLabel.setPixmap(QtGui.QPixmap("../../Pictures/upec.png"))
-        self.logLabel.setObjectName("logLabel")
-        self.quitBtn = QtWidgets.QPushButton(self.groupBox)
-        self.quitBtn.setGeometry(QtCore.QRect(140, 220, 89, 25))
-        self.quitBtn.setObjectName("quitBtn")
+        font.setPointSize(20)
+        self.textLabel.setFont(font)
+        self.textLabel.setObjectName("textLabel")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1319, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.videoGroupBox.setTitle(_translate("Dialog", "DISPLAY"))
-        self.startBtn.setText(_translate("Dialog", "START"))
-        self.labelTheme.setText(_translate("Dialog", "FACE RECOGNITION FOR ATTENDANCE MANAGEMENT"))
-        self.resultGroupBox.setTitle(_translate("Dialog", "RESULTS"))
-        self.quitBtn.setText(_translate("Dialog", "STOP"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "GroupBox"))
+        self.startPushButton.setText(_translate("MainWindow", "START"))
+        self.stopPushButton.setText(_translate("MainWindow", "STOP"))
+        self.displayResultGoupBox.setTitle(_translate("MainWindow", "RESULTS"))
+        self.textLabel.setText(_translate("MainWindow", "ATTENDANCE MANAGEMENT SYSTEM"))
+
+
+
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
