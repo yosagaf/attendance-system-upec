7df22a5665d6f1541990f426381026d78af8974e
yosagaf <sagafysf@gmail.com>
2021-01-01 12:47:26 +0100

    adding time and date

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from gui import Ui_MainWindow
+import datetime
 
 from read_video import VideoThread
 from knn_classifier import display_information
@@ -18,6 +19,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     
         self.Video = VideoThread()
         self.open_video()
+        #self.display_date_time()
+
+        timer = QTimer(self, interval=1000, timeout=self.show_time)
+        timer.start()
+        self.show_time()
        
     def open_video(self):
         
@@ -51,10 +57,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def on_quit_pushbutton_clicked(self):
         self.close()
 
+    @pyqtSlot()
+    def show_time(self): 
+        time = QTime.currentTime()
+        text = time.toString("HH:mm" if time.second() % 2 == 0 else "HH:mm")
+        self.time_label.setText(text)
+        
+        #self.time_date_label.setText('%s/%s/%s' % (date_time.month, date_time.day, date_time.year))
+        #self.time_date_label.setText(_translate("Form",now.strftime("%Y-%m-%d %H:%M"), None))
+        #self.text_browser_time.setText('%s:%s:%s' % (date_time.hour, date_time.minute, date_time.second))
+
 if __name__ == "__main__":
+    
     import sys
+    
     app = QtWidgets.QApplication(sys.argv)
     ui = MainWindow()
+    ui.setWindowTitle("ATTENDANCE MANAGEMENT SYSTEM")
     ui.show()
-    sys.exit(app.exec_())
+    
+    try:
+        sys.exit(app.exec_())
+    except:
+        print("Exiting")
     
